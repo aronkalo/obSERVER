ad8a7ebc96a1e94998e89f4c898d669ea48a1d09
aronkalo <kalo.aron@outlook.hu>
2020-03-29 14:34:28 +0200

    obServerModel started

---
+++ OENIK_PROG4_2020_1_GY2KVO_X7IKPT/obServer.Model/GameModel/Item/StaticItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace obServer.Model.GameModel.Item
+{
+    public sealed class StaticItem : BaseItem
+    {
+        public StaticItem(Geometry geometry, Guid id, double[] position, double rotation, bool impact) : base(geometry, id, position, rotation, impact)
+        {
+        }
+    }
+}
--- OENIK_PROG4_2020_1_GY2KVO_X7IKPT/obServer.Model/GameModel/obServerModel.cs
+++ OENIK_PROG4_2020_1_GY2KVO_X7IKPT/obServer.Model/GameModel/obServerModel.cs
@@ -1,12 +1,119 @@
 ﻿using System;
+using obServer.Model.GameModel.Item;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace obServer.Model.GameModel
 {
     public sealed class obServerModel
     {
+        public obServerModel()
+        {
+            Items = new List<BaseItem>();
+        }
+
+        private List<BaseItem> Items;
+
+        private bool itemsChanged
+        {
+            get { return itemsChanged; }
+            set
+            {
+                staticChanged = value;
+                playerChanged = value;
+                colliderChanged = value;
+                bulletChanged = value;
+                itemsChanged = value;
+            }
+        }
+
+        public IEnumerable<Player> Players
+        {
+            get
+            {
+                if (playerChanged)
+                {
+                    playerCache = (IEnumerable<Player>)Items.Where(x => x.GetType() == typeof(Player));
+                    playerChanged = false;
+                }
+                return playerCache;
+            }
+        }
+
+        public IEnumerable<Bullet> Bullets
+        {
+            get
+            {
+                if (bulletChanged)
+                {
+                    bulletCache = (IEnumerable<Bullet>)Items.Where(x => x.GetType() == typeof(Player));
+                    bulletChanged = false;
+                }
+                return bulletCache;
+            }
+        }
+
+        public IEnumerable<BaseItem> Colliders
+        {
+            get
+            {
+                if (colliderChanged)
+                {
+                    colliderCache = Items.Where(x => x.Impact);
+                    colliderChanged = false;
+                }
+                return colliderCache;
+            }
+        }
+
+        public IEnumerable<StaticItem> Statics
+        {
+            get
+            {
+                if (staticChanged)
+                {
+                    staticCache = (IEnumerable<StaticItem>)Items.Where(x => x.GetType() == typeof(StaticItem));
+                    staticChanged = false;
+                }
+                return staticCache;
+            }
+        }
+
+        public void ConstructItem(BaseItem item)
+        {
+            if (!Items.Contains(item))
+            {
+                Items.Add(item);
+                itemsChanged = true;
+            }
+        }
+
+        public void DestructItem(Guid id)
+        {
+            var items = Items.Where(x => x.Id == id);
+            if (items.Count() == 1)
+            {
+                BaseItem item = items.First();
+                Items.Remove(item);
+                item = null;
+                itemsChanged = true;
+            }
+        }
+
+        private IEnumerable<StaticItem> staticCache;
+
+        private bool staticChanged;
+
+        private IEnumerable<BaseItem> colliderCache;
+
+        private bool colliderChanged;
+
+        private IEnumerable<Bullet> bulletCache;
+
+        private bool bulletChanged;
+
+        private IEnumerable<Player> playerCache;
+
+        private bool playerChanged;
     }
 }
